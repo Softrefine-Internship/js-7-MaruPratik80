90cc37135239f6e012ea28af67599fbcf743258c
MaruPratik80 <pratikmaru80@.com>
2024-09-23 20:13:36 +0530

    changes

--- script.js
+++ script.js
@@ -4,17 +4,15 @@ const form = document.querySelector('.form-quiz-options');
 const quizQuestions = document.querySelector('.quiz-questions');
 const quizResults = document.querySelector('.quiz-results');
 const spinner = document.querySelector('.spinner');
-const errorModel = document.querySelector('.error-model');
-const overlay = document.querySelector('.overlay');
 
 const formInputs = document.querySelectorAll('.form__input');
+const categoryInput = document.getElementById('category');
 
 const btnStartQuiz = document.querySelector('.btn--start-quiz');
-const btnQuit = document.querySelector('.btn--quit');
+const btnQuitQuiz = document.querySelector('.btn--quit-quiz');
 const btnNext = document.querySelector('.btn--next');
 const btnShowResults = document.querySelector('.btn--results');
 const btnPlayNew = document.querySelector('.btn--play-new');
-const btnCloseError = document.querySelector('.btn-close-error');
 
 const questionNoEl = document.querySelector('.question-no');
 const scoreEl = document.querySelector('.score');
@@ -23,23 +21,48 @@ const difficultyEl = document.querySelector('.difficulty');
 const questionEl = document.querySelector('.question');
 const answersEl = document.querySelector('.answers');
 const totalScoreEL = document.querySelector('.total-score__value');
-const errorMessageEl = document.querySelector('.error-message');
+
+const model = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const modelMessageEl = document.querySelector('.model__message');
+const modelBtnsEL = document.querySelector('.model__btns');
+const btnContinue = document.querySelector('.btn--continue');
+const btnQuit = document.querySelector('.btn--quit');
+const btnCloseModel = document.querySelector('.btn-close-model');
 
 class QuizApp {
   #questions = [];
-  #currentQuestion = {};
   #currentQuestionIndex = 0;
+  #correctAnswer;
   #score = 0;
 
   constructor() {
+    this.#init();
+  }
+
+  async #init() {
+    // Event handlers
     btnStartQuiz.addEventListener('click', this.#startQuiz.bind(this));
     btnNext.addEventListener('click', this.#updateQuestion.bind(this));
     btnShowResults.addEventListener('click', this.#showResults.bind(this));
-    [btnQuit, btnPlayNew].forEach(btn => btn.addEventListener('click', this.#newQuiz.bind(this)));
-
-    [btnCloseError, overlay].forEach(btn => btn.addEventListener('click', this.#closeErrorModel));
-
+    btnPlayNew.addEventListener('click', this.#newQuiz.bind(this));
+    btnQuitQuiz.addEventListener('click', this.#confirmQuit.bind(this));
+    btnQuit.addEventListener('click', this.#quitQuiz.bind(this));
+    [btnCloseModel, btnContinue, overlay].forEach(btn => btn.addEventListener('click', this.#closeModel));
     answersEl.addEventListener('click', this.#checkAnswer.bind(this));
+
+    // Category
+    try {
+      const res = await fetch('https://opentdb.com/api_category.php');
+      if (!res.ok) throw new Error('No internet connection');
+      const { trivia_categories: category } = await res.json();
+      const markup = category
+        .map(cat => `<option value="${cat.id}">${cat.name.replace('Entertainment: ', '')}</option>`)
+        .join('');
+      categoryInput.firstElementChild.insertAdjacentHTML('afterend', markup);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   async #startQuiz(e) {
@@ -86,7 +109,7 @@ class QuizApp {
           return {
             type: result.type,
             difficulty: result.difficulty,
-            category: result.category,
+            category: result.category.replace('Entertainment: ', ''),
             question: result.question,
             correctAnswer: result.correct_answer,
             answers: [result.correct_answer, ...result.incorrect_answers].sort(() => Math.random() - 0.5),
@@ -105,7 +128,8 @@ class QuizApp {
       btnShowResults.classList.remove('hidden');
     }
 
-    const que = (this.#currentQuestion = this.#questions[this.#currentQuestionIndex]);
+    const que = this.#questions[this.#currentQuestionIndex];
+    this.#correctAnswer = que.correctAnswer;
 
     questionNoEl.textContent = `Question ${this.#currentQuestionIndex + 1} out of ${this.#questions.length}`;
     scoreEl.textContent = `Score: ${this.#score}`;
@@ -123,17 +147,18 @@ class QuizApp {
     const answer = e.target.closest('.answer');
     if (!answer) return;
 
-    const que = this.#currentQuestion;
-    const classToAdd = answer.innerHTML === que.correctAnswer ? 'correct' : 'incorrect';
+    const answers = Array.from(answersEl.children);
+
+    const classToAdd = answer.innerHTML === this.#correctAnswer ? 'correct' : 'incorrect';
     answer.classList.add(classToAdd);
 
     if (classToAdd === 'incorrect') {
-      Array.from(answersEl.children)
-        .find(ans => ans.innerHTML === que.correctAnswer)
-        .classList.add('correct');
+      answers.find(ans => ans.innerHTML === this.#correctAnswer).classList.add('correct');
     } else {
       this.#score++;
     }
+    scoreEl.textContent = `Score: ${this.#score}`;
+    answers.forEach(ans => (ans.disabled = true));
 
     btnNext.disabled = btnShowResults.disabled = false;
   }
@@ -155,18 +180,29 @@ class QuizApp {
   }
 
   #renderError(err) {
-    errorModel.classList.remove('hide-err');
-    overlay.classList.remove('hide-err');
-    errorMessageEl.textContent = `⚠️ Error: ${err.message}`;
-
+    this.#showModel(`⚠️ Error: ${err.message}`);
+    modelBtnsEL.classList.add('hidden');
     form.classList.remove('hidden');
     spinner.classList.add('hidden');
   }
-  #closeErrorModel() {
-    errorModel.classList.add('hide-err');
-    overlay.classList.add('hide-err');
+  #showModel(message) {
+    model.classList.remove('hide');
+    overlay.classList.remove('hide');
+    modelMessageEl.textContent = message;
+  }
+  #closeModel() {
+    model.classList.add('hide');
+    overlay.classList.add('hide');
     formInputs[0].value = 5;
     formInputs[0].focus();
+  }
+  #confirmQuit() {
+    this.#showModel('⚠️ Are you sure you want to quit quiz?');
+    modelBtnsEL.classList.remove('hidden');
+  }
+  #quitQuiz() {
+    this.#closeModel();
+    this.#newQuiz();
   }
 }
 
